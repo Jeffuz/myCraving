5158d585a39bbccaf47c53a5a5b2c3b9bf5d82d1
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-07-30 00:15:12 +0800

    setup nextjs + mui

---
+++ my-app/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "myCraving",
+  description: "A pantry management app to track and update your ingredients, featuring data analytics and AI-driven recipe generation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Navbar/>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
